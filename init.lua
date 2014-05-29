db3220c4f69719675d9a716febb3595ea21fee71
HybridDog <adresse_test@t-online.de>
2014-05-29 20:12:25 +0200

    add an undigable node instead of stone

--- init.lua
+++ init.lua
@@ -2,32 +2,50 @@ minetest.register_on_mapgen_init(function(mgparams)
 	minetest.set_mapgen_params({mgname="singlenode"})
 end)
 
-local c_stone = minetest.get_content_id("default:stone")
+minetest.register_node("pyripat:undigable", {
+	drawtype = "airlike",
+	pointable = false,
+	groups = {no=1},
+	drop = "",
+})
 
-minetest.register_on_generated(function(minp, maxp, seed)
+local c_undigable = minetest.get_content_id("pyripat:undigable")
+
+local function generate_chunk(minp, maxp, seed, generated)
 	if minp.y >= 10 then --avoid big map generation
 		return
 	end
 
-	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
-	local data = vm:get_data()
-	local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+	local vm, emin, emax, data, area
+	if generated then
+		vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+		area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+	else
+		vm = minetest.get_voxel_manip()
+		local emerged_pos1, emerged_pos2 = vm:read_from_map(minp, maxp)
+		area = VoxelArea:new({MinEdge=emerged_pos1, MaxEdge=emerged_pos2})
+	end
+	data = vm:get_data()
 
 	pr = PseudoRandom(seed+33)
 
-	for z = minp.z, maxp.z do
-		for y = minp.y, maxp.y do
-			for x = minp.x, maxp.x do
-				if y <= -8 then
-					data[area:index(x, y, z)] = c_stone
+	for y = minp.y, maxp.y do
+		if y == -8 then
+			for z = minp.z, maxp.z do
+				for x = minp.x, maxp.x do
+					data[area:index(x, y, z)] = c_undigable
 				end
 			end
 		end
 	end
 
 	vm:set_data(data)
---	vm:set_lighting(12)
---	vm:calc_lighting()
---	vm:update_liquids()
 	vm:write_to_map()
+	if not generated then
+		manip:update_map()
+	end
+end
+
+minetest.register_on_generated(function(minp, maxp, seed)
+	generate_chunk(minp, maxp, seed, true)
 end)
